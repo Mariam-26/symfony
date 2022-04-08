9f428c40fc865be3bda5f5f7e370041cd812c1cd
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-04-08 04:30:22 +0200

    symfony

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Articles;
+use App\Form\ArticlesFormType;
 use App\Repository\ArticlesRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -43,4 +46,44 @@ class BlogController extends AbstractController
              'article' => $article,
          ));
      }
+
+     /**
+      * @Route("/blog/new", name="new")
+      * @Route("/blog/edit/(id", name="edit")
+      */
+
+      public function create(Articles $articleNew = null, Request $requete, EntityManagerInterface $manager) : Response
+      {
+        if(!$articleNew) {
+            $articleNew = new Articles();
+        }
+
+        $form = $this->createForm(ArticlesFormType::class, $articleNew);
+        // ici on met en paramètres la clsse d'après laquelle on veut créer notre formulaire
+
+        $form->handleRequest($requete);
+
+        // on pioche la méthode handleRequest() de la class Request du composant HTTPFondation => ça va ns permettre de récupére chaque saisie facile ds le formulaire $_POST['article'] etc. et de ls lier directemt ds $articleNew
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            if(!$articleNew->getId()) /* si l'id n'exist ps ce n'est pas une modification, c'est un ajout d'article */ 
+            {
+                $articleNew->setCreatedAt(new \DateTime()); /* met à la date et à l'heure de la publication */
+            }
+
+            $manager->persist($articleNew); /* on met ent memoire ls données récupérés ds la formulaire */
+
+            $manager->flush(); /* envoi ds données ds la BDD */
+
+            return $this->redirectToRoute('show', [
+                'id' => $articleNew->getId(), /* on redirige vers la page show de l'article créé */
+            ]);
+        }
+
+        return $this->render('blog/new.html.twig', [
+            'form' => $form->createView(), /* ici on renvoie le formulaire avec ts ls champs requis pr l'insertion en BDD et on renvoie une vue gr^ce au createView() */
+            'modeEdit' => $articleNew->getId(),
+        ]);
+      }
 }
