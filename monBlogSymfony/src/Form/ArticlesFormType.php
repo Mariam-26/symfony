91b985a0f2e4accb2894b6385c0cd32b87a032c7
Mariam-26 <82433410+Mariam-26@users.noreply.github.com>
2022-04-21 04:33:08 +0200

    symfony

---
+++ monBlogSymfony/src/Form/ArticlesFormType.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Articles;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class ArticlesFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class)/* pr ajouter ds champd */
+            ->add('image', TextType::class)
+            ->add('content', TextType::class)
+            // ->add('createdAt')
+            ->add('author', TextType::class)
+            // ->add('submit', SubmitType::class)
+
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Articles::class,
+        ]);
+    }
+}
